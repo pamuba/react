e5e11f0c26fd2498c82d6e5c1d3815c1c9da9bca
pamuba <pamuba@users.noreply.github.com>
2020-07-09 23:56:44 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
+import Greet from './components/Greet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+          <div className="App">
+           <h3>Hello World from CLASS</h3>
+           <Greet></Greet>
+          </div>
+        );
+  }
 }
-
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Greet Component</h1>
+// }
+
+const Greet = () => <h1>Greet Component</h1>
+
+
+export default Greet
